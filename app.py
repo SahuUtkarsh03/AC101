8b94cc1722d834a5af5f5df952bdcce2ea31098a
Utkarsh Sahu <74239122+SahuUtkarsh03@users.noreply.github.com>
2021-07-28 16:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ st.write("""
 # Molecular Solubility Prediction Web App
 This app predicts the **Solubility (LogS)** values of molecules!
 Data obtained from the John S. Delaney. [ESOL:  Estimating Aqueous Solubility Directly from Molecular Structure](https://pubs.acs.org/doi/10.1021/ci034243x). ***J. Chem. Inf. Comput. Sci.*** 2004, 44, 3, 1000-1005.
-*** . To read about the background and report of this project please [click here.](https://t.ly/1lSm)
+To read about the background and report of this project please [click here.](https://t.ly/1lSm)
+***
 """)
 
 st.sidebar.header('User Input Features')
